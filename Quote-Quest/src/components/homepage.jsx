73e0226381dc1f84b349e82d2e3468aa308dd02b
Avionte Williams <aviontewilliams@gmail.com>
2025-03-20 17:12:25 -0400

    routes

--- Quote-Quest/src/components/homepage.jsx
+++ Quote-Quest/src/components/homepage.jsx
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom";
+
 export const HomepageTitle = () => {
   return (
-    <div className="title">
-      {" "}
-      <h2>Quote Quest</h2>
-      <p>
-        Those who Quote shape the world. Want to know who's shaping your world?
-      </p>
+    <div>
+      <div className="title">
+        {" "}
+        <h2>Quote Quest</h2>
+        <p>
+          Those who Quote shape the world. Want to know who's shaping your
+          world?
+        </p>
+      </div>
+      <button className="GamestartBtn">
+        <Link to="/Quest">Quest Start</Link>
+      </button>
+      <button className="GamestartBtn">
+        <Link to="/DailyQuote">Quote of the Day</Link>
+      </button>
     </div>
   );
 };
